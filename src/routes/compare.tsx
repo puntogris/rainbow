cb2753597873b1ee3040dac821962191fd97279f
puntogris <rosariodota3@gmail.com>
2024-08-13 20:20:56 -0300

    imp: now using random tailwind color as the initial comparation

--- src/routes/compare.tsx
+++ src/routes/compare.tsx
@@ -1,27 +1,19 @@
 import { createSignal, Signal } from 'solid-js';
+import tailwindColors from '../data/tailwind-colors.json';
 
 export default function Compare() {
 	const [colors, setColors] = createSignal<{ hex: string }[]>([
-		{ hex: '#121212' },
-		{ hex: '#112211' },
-		{ hex: '#000' }
+		{ hex: getRandomTailwindColor() },
+		{ hex: getRandomTailwindColor() }
 	]);
-	const colorsRandom = [
-		'#E63946',
-		'#F1FAEE',
-		'#A8DADC',
-		'#457B9D',
-		'#1D3557',
-		'#FFBE0B',
-		'#FB5607',
-		'#FF006E',
-		'#8338EC',
-		'#3A86FF'
-	];
-	function addColor(index: number) {
-		const randomColor = colorsRandom[Math.floor(Math.random() * colorsRandom.length)];
-		colors().splice(index + 1, 0, { hex: randomColor });
 
+	function getRandomTailwindColor() {
+		const randomColor = tailwindColors[Math.floor(Math.random() * tailwindColors.length)];
+		return randomColor.hex;
+	}
+
+	function addColor(index: number) {
+		colors().splice(index + 1, 0, { hex: getRandomTailwindColor() });
 		setColors([...colors()]);
 	}
 
@@ -61,7 +53,7 @@ export default function Compare() {
 							<div class="absolute left-full top-1/2 z-10 flex h-full w-1/4 -translate-x-1/2 -translate-y-1/2 transform items-center justify-center p-2 text-white opacity-0 hover:opacity-100">
 								<button
 									onClick={() => addColor(index)}
-									class="h-12 w-12 rounded-full bg-white p-2 text-lg text-black"
+									class="h-12 w-12 rounded-full bg-white p-2 text-lg text-black shadow"
 								>
 									+
 								</button>
@@ -71,7 +63,7 @@ export default function Compare() {
 							<div class="absolute right-0 top-1/2 z-10 flex h-full w-1/4 -translate-y-1/2 transform items-center justify-center p-2 text-white opacity-0 hover:opacity-100">
 								<button
 									onClick={() => addColor(index)}
-									class="h-12 w-12 rounded-full bg-white p-2 text-lg text-black"
+									class="h-12 w-12 rounded-full bg-white p-2 text-lg text-black shadow"
 								>
 									+
 								</button>
