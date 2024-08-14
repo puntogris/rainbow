28f8bfae3855e3aa8732f16ece4960965ee1bbd5
puntogris <rosariodota3@gmail.com>
2024-08-13 21:25:42 -0300

    feature: new 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,11 +1,22 @@
-import RainbowIcon from '~/icons/rainbowIcon';
-
 export default function NotFound() {
 	return (
-		<main class="flex grow flex-col items-center justify-center p-4 text-gray-700">
-			<RainbowIcon class="mx-auto h-24 w-24 text-white" />
-			<h1 class="text-2xl font-medium uppercase text-sky-300">Not Found</h1>
-			<p class="text-white">The page you are looking for does not exist.</p>
+		<main class="relative flex grow flex-col items-center justify-center">
+			<div class="absolute inset-0 bg-gradient-to-r from-rose-500 via-blue-500 to-purple-500" />
+			<div class="relative flex flex-col items-center justify-center gap-2 rounded px-5 py-3 text-center">
+				<div class="flex items-center gap-2">
+					<h1 class="text-4xl font-medium text-white">Oops, looks like the end of the rainbow!</h1>
+				</div>
+				<p class="text-lg text-white">
+					The page you’re looking for isn’t here. Maybe it’s hidden in the next rainbow over?
+				</p>
+			</div>
+			<a
+				href="https://www.puntogris.com/"
+				target="_blank"
+				class="absolute bottom-0 right-0 p-2 text-sm text-zinc-800 hover:underline"
+			>
+				puntogris enterprises ↗
+			</a>
 		</main>
 	);
 }
